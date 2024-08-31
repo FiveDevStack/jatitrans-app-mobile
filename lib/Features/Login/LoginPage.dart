b85bc0a095eb1c5643e77499aa90acaf22686c94
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2024-08-31 23:10:20 +0700

    add login page

---
+++ lib/Features/Login/LoginPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jatitrans_app/Features/Login/Controller/LoginPageController.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var _controller = Get.put(LoginPageController());
+    return const Placeholder();
+  }
+}
